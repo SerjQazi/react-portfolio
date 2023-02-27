d29694be41c8f99aabfd27a5d71850e91eadc30d
serjqazi <serjqazi@Serjs-MacBook-Pro.local>
2023-02-26 23:27:42 -0500

    about page in progress

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Skills from './Skills';
 
 const About = () => {
   return (
       <div className='flex justify-center'>
         <div className="wrapper w-10/12">
-          <div className='flex justify-center items-center border-2 border-dashed border-slate-700 border-opacity-50 my-8'>
-            <h1 className='uppercase text-center font-bold text-slate-700 text-8xl py-6'>
+          <div className='flex justify-center items-center border-2 border-dashed border-slate-700 border-opacity-50 my-7'>
+            <h1 className='uppercase text-center font-bold text-slate-700 text-8xl py-4'>
               About Me
             </h1>
           </div>
 
-          <div className='text-slate-700 mb-6'>
-            <h2 className='text-2xl mb-4'>I'm <span className='font-bold'>Serj Qazi,</span> <span className='font-light'>Frontend Developer / Graphic Designer</span></h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officiis distinctio officia recusandae molestiae vitae id temporibus amet magnam. Temporibus magnam nam error beatae dolore veritatis praesentium, accusantium accusamus maxime!</p>
+          <div className='text-slate-700 mb-5'>
+            <h2 className='text-2xl mb-4 font-light'>I'm <span className='font-bold'>Serj Qazi,</span> Frontend Developer / Graphic Designer</h2>
+
+            <p>Frontend Developer with years of experience in building accessible and responsive web pages using React. I have worked with RESTful APIs, Git, and Figma, and have experience in agile methodologies such as Kanban, Jira, and Confluence. Additionally, I have experience with Material-UI, Ant Design, Tailwind, and Firebase, and have worked on projects in small and large team environments.</p>
           </div>
 
-          <div className="grid grid-cols-3 h-[25vh]">
+          <div className="grid grid-cols-3">
             
-            <div className="col-span-2 leftSide border-2 border-blue-500">
-
+            <div className="col-span-2 bg-slate-700 drop-shadow-lg">
+              <Skills />              
             </div>
 
             <div className="col-span-1 rightSide border-2 border-green-400">
---
+++ src/pages/about/Skills.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { FaReact, SiTailwindcss, SiJavascript, SiJquery, SiHtml5, SiCss3, FaSass, SiGithub, SiFirebase, FaDatabase, VscDebugAll, IoMdTabletPortrait, FaUniversalAccess } from 'react-icons/all';
+
+const Skills = () => {
+    return (
+        <>
+            <div className='flex justify-center items-center border-2 border-dashed border-slate-400 border-opacity-50 mx-36 my-4'>
+                <h3 className='uppercase text-yellow-500 text-2xl py-3'>Skills</h3>
+            </div>
+
+            <div className="text-yellow-500 text-4xl">
+                <ul className='grid grid-cols-6'>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <FaReact />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>React</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <SiJavascript />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>JavaScript</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <SiJquery />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>jQuery</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <SiHtml5 />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>HTML 5</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <SiCss3 />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>CSS 3</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <FaSass />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>SASS</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <SiTailwindcss />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>Tailwind</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <SiGithub />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>GitHub</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <SiFirebase />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>Firebase</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <FaDatabase />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>RESTful API</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <VscDebugAll />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>Debugging</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center'>
+                    <IoMdTabletPortrait />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>Responsive</p>
+                    </li>
+                    <li className='mb-4 flex flex-col justify-center items-center col-span-6'>
+                    <FaUniversalAccess />
+                    <p className='text-xs text-center font-thin mt-3 text-slate-100'>A11Y</p>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Skills
