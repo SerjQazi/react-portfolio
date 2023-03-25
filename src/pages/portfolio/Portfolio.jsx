872c0531d4b421d640b7fbb77e7eb6c0b9915b36
serjqazi <serjqazi@Serjs-MacBook-Pro.local>
2023-03-25 07:18:11 -0400

    project page build

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -22,10 +22,12 @@ const Portfolio = () => {
   const desktop = 'uppercase text-center font-black tracking-widest text-slate-700 phone:text-xl sm:text-2xl lg:text-6xl xl:text-5xl 2xl:text-6xl py-6 phone:py-2'
 
   return (
-      <div className='flex justify-center items-center bg-slate-100 border-2 border-red-500'>
+      <div className='flex justify-center items-center bg-slate-100'>
         <div className="wrapper w-full lg:w-10/12 px-2 phone:h-auto sm:px-4 phone:px-0 phone:pr-2">
 
-          <div className='flex flex-row-reverse justify-between sm:flex-col mt-4 mb-2 phone:mb-2 sm:mb-4'>
+          <div className='mb-4'>
+
+            <div className='flex flex-row-reverse justify-between sm:flex-col mt-4 mb-2 phone:mb-2 sm:mb-4'>
             <div className='absolute sm:static flex justify-center items-center border-2 border-dashed border-slate-700 border-opacity-50 ml-2 sm:mx-28 xl:mx-48 sm:mb-4 lg:mt-10 lg:mb-8'>
               <h1 className={width < breakpoint ? mobile : desktop}>
                 Portfolio
@@ -44,9 +46,9 @@ const Portfolio = () => {
               <div className='infoContainer w-11/12 ml-4 mr-14 md:mr-0'>
 
                 <div className=''>
-                  <h2 className='font-bold text-xl tracking-widest'>Robo Trivia</h2>
-                  <h3 className='font-thin text-xs tracking-wide mb-2'>React | JavaScript | HTML5 | SASS | Firebase</h3>
-                  <p className='font-thin text-base tracking-wide md:mr-2'>A solo or multiplayer trivia game that uses an API to generate questions & multiple choice answers. A simpler second API is used to generate the avatars for each unique username and finally firebase stores all of the user data and assigns the score to each user's unique key.</p>
+                  <h2 className='font-bold text-2xl tracking-widest'>Robo Trivia</h2>
+                  <h3 className='font-thin text-sm tracking-wide mb-4'>React | JavaScript | HTML5 | SASS | Firebase</h3>
+                  <p className='font-thin text-lg tracking-normal md:mr-2'>A solo or multiplayer trivia game that uses an API to generate questions & multiple choice answers. A simpler second API is used to generate the avatars for each unique username and finally firebase stores all of the user data and assigns the score to each user's unique key.</p>
                 </div>
 
                 <div className='buttonContainer flex mt-4'>
@@ -75,7 +77,7 @@ const Portfolio = () => {
 
             </div>
 
-            <div className='findMyBeerContainer flex justify-center items-center mb-10'>
+            <div className='findMyBeerContainer flex justify-center items-center mb-0 md:mb-10'>
               <div className='w-1/3 hidden'>
                 <img src={roboTrivia} alt="" />
               </div>
@@ -83,9 +85,9 @@ const Portfolio = () => {
               <div className='infoContainer w-11/12 ml-4'>
 
                 <div className=''>
-                  <h2 className='font-bold text-xl tracking-widest'>Find My Beer</h2>
-                  <h3 className='font-thin text-xs tracking-wide mb-2'>React | JavaScript | HTML5 | SASS</h3>
-                  <p className='font-thin text-base tracking-wide'>A way to find all the breweries in your state, the API generates a list of breweries once you pick a state. The front of the card displays name and address, and as you hover over each card the back shows brewery type, contact info and brewery website.</p>
+                  <h2 className='font-bold text-2xl tracking-widest'>Find My Beer</h2>
+                  <h3 className='font-thin text-sm tracking-wide mb-4'>React | JavaScript | HTML5 | SASS</h3>
+                  <p className='font-thin text-lg tracking-normal'>A way to find all the breweries in your state, the API generates a list of breweries once you pick a state. The front of the card displays name and address, and as you hover over each card the back shows brewery type, contact info and brewery website.</p>
                 </div>
 
                 <div className='buttonContainer flex mt-4'>
@@ -114,7 +116,7 @@ const Portfolio = () => {
 
             </div>
 
-            <div className='helperContainer flex justify-center items-center mb-10'>
+            <div className='helperContainer hidden md:flex justify-center items-center'>
               <div className='w-1/3 hidden'>
                 <img src={roboTrivia} alt="" />
               </div>
@@ -154,6 +156,10 @@ const Portfolio = () => {
             </div>
 
           </div>
+
+          </div>
+
+          
           {/* end of lower section */}
         </div>
         {/* end of wrapper */}
