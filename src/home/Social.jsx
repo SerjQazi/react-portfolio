f1b6d34dc25698375250d38eeca567dd9f97064c
serjqazi <serjqazi@Serjs-MacBook-Pro.local>
2023-03-25 04:16:27 -0400

    mobile responsiveness completed

--- src/home/Social.jsx
+++ src/home/Social.jsx
@@ -11,31 +11,31 @@ const Social = () => {
     <div className='w-full h-full ml-2 sm:ml-4 mr-1 sm:mr-4 3xl:mr-6'>
       <ul className='w-full h-5/6 my-10 flex flex-col justify-center items-center rounded-lg bg-yellow-500'>
         
-        <li className='flex justify-center items-center w-full h-full px-1 sm:px-2 cursor-pointer rounded-tr-lg rounded-tl-lg border-b-2 border-dashed border-slate-700  hover:bg-slate-700 duration-300 text-slate-600 hover:text-yellow-500'>
+        <li className='flex justify-center items-center w-full h-full cursor-pointer rounded-tr-lg rounded-tl-lg border-b-2 border-dashed border-slate-700  hover:bg-slate-700 duration-300 text-slate-600 hover:text-yellow-500'>
           
-          <Link className='flex justify-center items-center w-full h-full py-1 2xl:py-2 text-3xl phone:text-2xl lg:text-3xl 2xl:text-4xl hover:scale-105 duration-300 focus:text-yellow-500 focus:outline-none focus:bg-slate-700 focus:rounded-bl-lg focus:rounded-tl-lg' 
+          <Link className='flex justify-center items-center w-full h-full py-1 px-2 2xl:py-2 text-3xl phone:text-2xl lg:text-3xl 2xl:text-4xl hover:scale-105 duration-300 focus:text-yellow-500 focus:outline-none focus:bg-slate-700 focus:rounded-tr-lg focus:rounded-tl-lg' 
           to='https://www.linkedin.com/in/serjqazi/' target='_blank' rel='noopener noreferrer'>
             <AiOutlineLinkedin/>
           </Link>
         
         </li>
 
-        <li className='flex justify-center items-center w-full h-full px-1 sm:px-2 cursor-pointer border-b-2 border-dashed border-slate-700  hover:bg-slate-700 duration-300 text-slate-600 hover:text-yellow-500'>
-          <Link className='flex justify-center items-center w-full h-full py-1 2xl:py-2 text-3xl phone:text-2xl lg:text-3xl 2xl:text-4xl hover:scale-105 duration-300 focus:text-yellow-500 focus:outline-none focus:bg-slate-700' 
+        <li className='flex justify-center items-center w-full h-full cursor-pointer border-b-2 border-dashed border-slate-700  hover:bg-slate-700 duration-300 text-slate-600 hover:text-yellow-500'>
+          <Link className='flex justify-center items-center w-full h-full py-1 px-2 2xl:py-2 text-3xl phone:text-2xl lg:text-3xl 2xl:text-4xl hover:scale-105 duration-300 focus:text-yellow-500 focus:outline-none focus:bg-slate-700' 
           to='https://github.com/SerjQazi' target='_blank' rel='noopener noreferrer'>
             <TbBrandGithub/>
           </Link>
         </li>
 
-        <li className='flex justify-center items-center w-full h-full px-1 sm:px-2 cursor-pointer border-b-2 border-dashed border-slate-700  hover:bg-slate-700 duration-300 text-slate-600 hover:text-yellow-500'>
-          <Link className='flex justify-center items-center w-full h-full py-1 2xl:py-2 text-3xl phone:text-2xl lg:text-3xl 2xl:text-4xl hover:scale-105 duration-300 focus:text-yellow-500 focus:outline-none focus:bg-slate-700' 
+        <li className='flex justify-center items-center w-full h-full cursor-pointer border-b-2 border-dashed border-slate-700  hover:bg-slate-700 duration-300 text-slate-600 hover:text-yellow-500'>
+          <Link className='flex justify-center items-center w-full h-full py-1 px-2 2xl:py-2 text-3xl phone:text-2xl lg:text-3xl 2xl:text-4xl hover:scale-105 duration-300 focus:text-yellow-500 focus:outline-none focus:bg-slate-700' 
           to='https://twitter.com/qazi_serj' target='_blank' rel='noopener noreferrer'>
             <RxTwitterLogo/>
           </Link>
         </li>
 
-        <li className='flex justify-center items-center w-full h-full px-1 sm:px-2 cursor-pointer rounded-br-lg rounded-bl-lg  hover:bg-slate-700 duration-300 text-slate-600 hover:text-yellow-500'>
-          <Link className='flex justify-center items-center w-full h-full py-1 2xl:py-2 text-3xl phone:text-2xl lg:text-3xl 2xl:text-4xl hover:scale-105 duration-300 focus:text-yellow-500 focus:outline-none focus:bg-slate-700 focus:rounded-br-lg focus:rounded-bl-lg' 
+        <li className='flex justify-center items-center w-full h-full cursor-pointer rounded-br-lg rounded-bl-lg  hover:bg-slate-700 duration-300 text-slate-600 hover:text-yellow-500'>
+          <Link className='flex justify-center items-center w-full h-full py-1 px-2 2xl:py-2 text-3xl phone:text-2xl lg:text-3xl 2xl:text-4xl hover:scale-105 duration-300 focus:text-yellow-500 focus:outline-none focus:bg-slate-700 focus:rounded-br-lg focus:rounded-bl-lg' 
           to={resume} target='_blank' rel='noopener noreferrer'>
             <BsFileEarmarkPdf/>
           </Link>
--- src/pages/contacts/ContactBox.jsx
+++ src/pages/contacts/ContactBox.jsx
@@ -24,68 +24,72 @@ const ContactBox = () => {
   return (
       <div className='flex flex-col justify-center items-center text-slate-700 phone:w-1/2 lg:w-7/12'>
 
-        <form className='flex flex-col justify-center items-center w-full'>
-          <label htmlFor="name" className='sr-only'>enter your name</label>
-            <input type='text' 
-            id='name' 
-            name='name' 
-            placeholder='name' 
-            className='w-full px-3 py-2 phone:py-1 lg:py-2 sm:p-4 mb-0 phone:mb-0 sm:mb-2 text-base phone:text-xs sm:text-xl lg:text-[1rem] xl:text-sm 2xl:text-lg bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500'
-            />
-
-          <label htmlFor="email" className='sr-only'>enter your email address</label>
-            <input 
-            type='email' 
-            id='email' 
-            name='email' 
-            placeholder='email@example.com' 
-            className='mt-2 w-full px-3 py-2 phone:py-1 lg:py-2 sm:p-4 mb-0 phone:mb-0 sm:mb-2 text-base phone:text-xs sm:text-xl lg:text-[1rem] xl:text-sm 2xl:text-lg bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 invalid:border-red-500 invalid:border-2'
-            />
-
-          {
-            width < breakpoint ?
-            <>
-              <label htmlFor="message" className='sr-only'>enter your message</label>
-              <textarea 
-              type='text' 
-              id='message' 
-              name='message' 
-              placeholder='message' 
-              cols="30" 
-              rows="8"
-              className='mt-2 w-full px-3 py-2 phone:py-1 lg:py-2 sm:p-4 text-base xl:text-sm 2xl:text-lg bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 resize-none'
+        <form className='flex flex-col justify-center items-center w-full' name="contact" netlify>
+          <fieldset>
+            <label htmlFor="name" className='sr-only'>enter your name</label>
+              <input type='text' 
+              id='name' 
+              name='name' 
+              placeholder='name' 
+              className='w-full px-3 py-2 phone:py-1 lg:py-2 sm:p-4 mb-0 phone:mb-0 sm:mb-2 text-base phone:text-xs sm:text-xl lg:text-[1rem] xl:text-sm 2xl:text-lg bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500'
               />
-            </>
-            : width == xlBreakpoint || width == xxlBreakpoint || width == iPadPro || width >= xxxlBreakpoint ?
-            <>
-              <label htmlFor="message" className='sr-only'>enter your message</label>
-              <textarea 
-              type='text' 
-              id='message' 
-              name='message' 
-              placeholder='message' 
-              cols="30" 
-              rows="6"
-              className='mt-2 w-full px-3 py-2 phone:py-1 lg:py-2 sm:p-4 text-base bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 resize-none'
-              />
-            </>
-            :
-            <>
-              <label htmlFor="message" className='sr-only'>enter your message</label>
-              <textarea 
-              type='text' 
-              id='message' 
-              name='message' 
-              placeholder='message' 
-              cols="30" 
-              rows="4"
-              className='mt-2 w-full px-3 py-1 text-base phone:text-xs sm:text-xl lg:text-[1rem] bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 resize-none'
-              />
-            </>
-            
-          }
 
-          <button className='px-5 py-1 sm:px-8 sm:py-3 phone:px-5 phone:py-1 lg:py-2 lg:px-6 2xl:py-3 2xl:px-10 mt-4 phone:mt-2 sm:mt-8 lg:mt-3 xl:mt-4 2xl:mt-5 bg-yellow-500 rounded-md uppercase font-bold tracking-widest flex justify-center items-center text-base phone:text-sm sm:text-2xl xl:text-sm 2xl:text-xl' type='submit'>Send<IoMdPaperPlane className='ml-1 sm:ml-3 phone:ml-1 text-base sm:text-2xl'/> </button>
+            <label htmlFor="email" className='sr-only'>enter your email address</label>
+              <input 
+              type='email' 
+              id='email' 
+              name='email' 
+              placeholder='email@example.com' 
+              className='mt-2 w-full px-3 py-2 phone:py-1 lg:py-2 sm:p-4 mb-0 phone:mb-0 sm:mb-2 text-base phone:text-xs sm:text-xl lg:text-[1rem] xl:text-sm 2xl:text-lg bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 invalid:border-red-500 invalid:border-2'
+              />
+
+            {
+              width < breakpoint ?
+              <>
+                <label htmlFor="message" className='sr-only'>enter your message</label>
+                <textarea 
+                type='text' 
+                id='message' 
+                name='message' 
+                placeholder='message' 
+                cols="30" 
+                rows="8"
+                className='mt-2 w-full px-3 py-2 phone:py-1 lg:py-2 sm:p-4 text-base xl:text-sm 2xl:text-lg bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 resize-none'
+                />
+              </>
+              : width == xlBreakpoint || width == xxlBreakpoint || width == iPadPro || width >= xxxlBreakpoint ?
+              <>
+                <label htmlFor="message" className='sr-only'>enter your message</label>
+                <textarea 
+                type='text' 
+                id='message' 
+                name='message' 
+                placeholder='message' 
+                cols="30" 
+                rows="6"
+                className='mt-2 w-full px-3 py-2 phone:py-1 lg:py-2 sm:p-4 text-base bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 resize-none'
+                />
+              </>
+              :
+              <>
+                <label htmlFor="message" className='sr-only'>enter your message</label>
+                <textarea 
+                type='text' 
+                id='message' 
+                name='message' 
+                placeholder='message' 
+                cols="30" 
+                rows="4"
+                className='mt-2 w-full px-3 py-1 text-base phone:text-xs sm:text-xl lg:text-[1rem] bg-slate-100 border border-slate-500 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 resize-none'
+                />
+              </>
+              
+            }
+
+            <div className="buttonContainer flex justify-center">
+              <button type='submit' className='px-5 py-1 sm:px-8 sm:py-3 phone:px-5 phone:py-1 lg:py-2 lg:px-6 2xl:py-3 2xl:px-10 mt-4 phone:mt-2 sm:mt-8 lg:mt-3 xl:mt-4 2xl:mt-5 bg-yellow-500 rounded-md uppercase font-bold tracking-widest flex justify-center items-center text-base phone:text-sm sm:text-2xl xl:text-sm 2xl:text-xl'>Send<IoMdPaperPlane className='ml-1 sm:ml-3 phone:ml-1 text-base sm:text-2xl'/> </button>
+            </div>
+          </fieldset>
         </form>
 
       </div>
