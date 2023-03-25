abbc22f8de78ccf55315a9ffb68b5f9582edf5ef
serjqazi <serjqazi@Serjs-MacBook-Pro.local>
2023-03-25 05:40:35 -0400

    mobile responsiveness completed

--- src/components/TopNavigation.jsx
+++ src/components/TopNavigation.jsx
@@ -11,27 +11,27 @@ const TopNavigation = () => {
     return (
         <div className='bg-yellow-500'>
             <ul className='uppercase font-sans font-medium text-3xl text-center text-slate-700 tracking-[0.35em] flex justify-center items-center'>
-                <li className='py-2 px-5 sm:px-11 md:px-[3.2rem] border-r border-slate-900 border-opacity-25'>
+                <li className='py-2 px-6 sm:px-11 md:px-[3.8rem] border-r border-slate-900 border-opacity-25'>
                     <NavLink to='/' className={({ isActive}) => isActive ? activeLink:normalLink}>
                         <FaHome />
                     </NavLink>
                 </li>
-                <li className='py-2 px-5 sm:px-11 md:px-[3.2rem] border-r border-slate-900 border-opacity-25'>
+                <li className='py-2 px-6 sm:px-11 md:px-[3.8rem] border-r border-slate-900 border-opacity-25'>
                     <NavLink to={"/profile/about"} className={({ isActive}) => isActive ? activeLink:normalLink}>
                         <BsFillPersonFill />
                     </NavLink>
                 </li>
-                <li className='py-2 px-5 sm:px-11 md:px-[3.2rem] border-r border-slate-900 border-opacity-25'>
+                <li className='py-2 px-6 sm:px-11 md:px-[3.8rem] border-r border-slate-900 border-opacity-25'>
                     <NavLink to={"/profile/resume"} className={({ isActive}) => isActive ? activeLink:normalLink}>
                         <VscFilePdf />
                     </NavLink>
                 </li>
-                <li className='py-2 px-5 sm:px-11 md:px-[3.2rem] border-r border-slate-900 border-opacity-25'>
+                <li className='py-2 px-6 sm:px-11 md:px-[3.8rem] border-r border-slate-900 border-opacity-25'>
                     <NavLink to={"/profile/portfolio"} className={({ isActive}) => isActive ? activeLink:normalLink}>
                         <CgCodeSlash />
                     </NavLink>
                 </li>
-                <li className='py-2 px-5 sm:px-11 md:px-[3.2rem]'>
+                <li className='py-2 px-6 sm:px-11 md:px-[3.8rem]'>
                     <NavLink to={"/profile/contacts"} className={({ isActive}) => isActive ? activeLink:normalLink}>
                         <TiContacts />
                     </NavLink>
